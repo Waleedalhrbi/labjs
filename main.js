29ca30bbccf679f1a8075842fabb3ffd390fc2db
Waleed Alhrbi <waleedalhrbi2001@gmail.com>
2024-09-08 12:07:07 +0300

    I have completed all tasks

---
+++ main.js
@@ -0,0 +1,62 @@
+/* Rectangle of Area */
+
+let width = 10;
+let higt = 5;
+
+let rectangleOfArea = width *higt;
+
+console.log(`The area of the rectangle is ${rectangleOfArea}`);
+
+/* Temperature converter from Celsius to Fahrenheit 8 */
+
+let Celsius = 45;
+let converterfromCtoF = (Celsius+ 32) * 9/5;
+
+let Fahrenheit = 104;
+let converterfromFtoC = (Fahrenheit- 32) * 5/9;
+
+
+
+console.log(`NN°F is ${converterfromCtoF} NN°C is ${converterfromFtoC}`);
+
+
+/* Convert from hours to seconds */
+
+let hour = 3;
+let second = 3 *3600;
+
+console.log(` ${hour} hours = ${second} Seconds `);
+
+
+/* thingOne thingTwo */
+let thingOne = "Waleed";
+let thingTwo = "Alhrbi";
+
+console.log(thingOne + " " + thingTwo);
+
+/* total of bill */
+
+var bill = 10.25 + 3.99 + 7.15;
+var tip = bill * 0.15;
+var total = bill + tip;
+console.log(`bill = ${bill} , tip = ${tip} , total = ${total}`);
+
+/* Game of Word */
+
+var adjective1 = "amazing";
+var adjective2 = "fun";
+var adjective3 = "entertaining";
+var madLib = `The Intro to JavaScript course is ${adjective1}. James and Julia are so ${adjective2}. I cannot wait to work through the rest of this ${adjective3} content!`;
+console.log(madLib);
+
+/* Awesome Message */
+
+var firstName = "Julia";
+var interest = "cats";
+var hobby = "play video games";
+
+var awesomeMessage = "Hi, my name is"+" "+ firstName +" I love"+ " "+ interest+" "+"In my spare time, I like to"+" "+ hobby;
+
+
+console.log(awesomeMessage)
+
